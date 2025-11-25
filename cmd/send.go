2e8aaa30fc817dbf55663f75e22d20161b7e2f9e
Wael Mahrous <wael.mahrous@svep.se>
2025-11-25 15:30:25 +0100

    fix(send): handle directories
    Use a third party plugin to handle copying, now supporting directories.

--- cmd/send.go
+++ cmd/send.go
@@ -4,6 +4,7 @@ Copyright © 2025 Wael Mahrous
 package cmd
 
 import (
+	"github.com/otiai10/copy"
 	"log"
 	"os"
 	"path/filepath"
@@ -21,22 +22,11 @@ var sendCmd = &cobra.Command{
 
 	Run: func(cmd *cobra.Command, args []string) {
 		target, _ := os.ReadFile(FilePath)
-
 		log.Println("sending", len(args), "file(s) to ", string(target))
 
 		if copyMode == true {
 			for _, fileName := range args {
-				input, err := os.ReadFile(fileName)
-				if err != nil {
-					log.Println(err)
-					return
-				}
-
-				err = os.WriteFile(filepath.Join(string(target), fileName), input, 0644)
-				if err != nil {
-					log.Println(err)
-					return
-				}
+				copy.Copy(fileName, filepath.Join(string(target), fileName))
 			}
 		} else {
 			for _, fileName := range args {
--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module github.com/waelmahrous/wormhole
 
 go 1.25.4
 
-require github.com/spf13/cobra v1.10.1
+require (
+	github.com/otiai10/copy v1.14.1
+	github.com/spf13/cobra v1.10.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/otiai10/mint v1.6.3 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 )
